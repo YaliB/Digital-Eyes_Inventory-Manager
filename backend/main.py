a05c3bd6b49c7ca83475c8572dd2984782bb121d
tamerab1 <tamerab200@gmail.com>
2026-04-30 03:12:43 +0300

    Add new allowed origin for CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://13.53.143.132:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
